e7190209361049cdc90855b9a5ce95cad78a7699
hpf-zzu <945928180@qq.com>
2020-06-15 19:53:22 +0800

    规则7测试通过

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -4,6 +4,17 @@ public class FizzBuzz {
 
 
     public String say(int number) {
+
+        if(String.valueOf(number).contains("7")){
+
+            String temp = "";
+            if(number % 3 == 0)
+                temp += "Fizz";
+            if(number % 7 == 0)
+                temp += "Whizz";
+            return temp;
+        }
+
         if(String.valueOf(number).contains("5")){
 
             String temp = "";
